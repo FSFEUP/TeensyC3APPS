9cb4b5d5e489124116c080cfe2f0e50c5ea122df
dvalinn <tiago.andre.amorim@gmail.com>
2023-05-03 23:22:09 +0100

    testing session 1 code

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define N_SAMPLES 10
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(A17, INPUT);
+    pinMode(A16, INPUT);
+}
+
+float desvio(int v1, int v2) {
+    int v_max = max(v1, v2);
+    int v_min = min(v1, v2) + 500;
+
+    return ((v_max - v_min) / (float)v_min) * 100;
+}
+
+void loop() {
+    int v_apps1 = 0;
+    int v_apps2 = 0;
+
+    for (int i = 0; i < N_SAMPLES; i++) {
+        v_apps1 += analogRead(A17);
+        v_apps2 += analogRead(A16);
+        delay(10);
+    }
+
+    v_apps1 /= N_SAMPLES;
+    v_apps2 /= N_SAMPLES;
+
+    desvio(v_apps1, v_apps2);
+
+    String output = String(v_apps1, DEC) + "\t" + String(v_apps2, DEC) + "\t" + desvio(v_apps1, v_apps2) + "%";
+    Serial.println(output);
+}
