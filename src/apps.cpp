06c3596c3805937ff6de858053d58fbbb9a925b3
dvalinn <tiago.andre.amorim@gmail.com>
2023-06-28 20:18:32 +0100

    code

--- src/apps.cpp
+++ src/apps.cpp
@@ -50,17 +50,11 @@ int read_apps() {
     int v_apps1 = analogRead(APPS_1_PIN);
     int v_apps2 = analogRead(APPS_2_PIN);
 
-    if (!plausibility(v_apps1, v_apps2))
-        return false;
-
     buffer_insert(avgBuffer1, AVG_SAMPLES, v_apps1);
     buffer_insert(avgBuffer2, AVG_SAMPLES, v_apps2);
 
     v_apps1 = average(avgBuffer1, AVG_SAMPLES);
     v_apps2 = average(avgBuffer2, AVG_SAMPLES);
 
-    if (!plausibility(v_apps1, v_apps2))
-        return -1;
-
-    return (BAMOCAR_MAX / 5) - (v_apps1 * (BAMOCAR_MAX / 5) / APPS_1_UPPER_BOUND);
+    return Convert_To_BAMOCAR_Scale();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,13 @@ void setup() {
 }
 
 void loop() {
-    // int apps_value_1 = analogRead(A17);
-    // int apps_value_2 = analogRead(A16);
     int apps_value = read_apps();
-    // if (apps_value > 0) {
+
+    if (apps_value > 0) {
         // curr_state = r2d_state_machine(curr_state, apps_value);
-    // } else {
-        // Serial.print("ERROR: apps_implausibility");
-    // }
+    } else {
+        Serial.println("ERROR: apps_implausibility");
+    }
     delay(1);
     // else
     // send_to_bamocar(0);
