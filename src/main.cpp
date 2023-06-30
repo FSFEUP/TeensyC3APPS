0369aa70b578b1cf7559a75bd41582abca840031
dvalinn <tiago.andre.amorim@gmail.com>
2023-06-30 10:58:49 +0100

    changes main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
+#include <elapsedMillis.h>
 
 #include "apps.h"
 #include "display.h"
 #include "r2d.h"
+#include "can.h"
+
+#define APPS_1_PIN 41
+#define APPS_2_PIN 40
+
+#define BAMOCAR_ATTENUATION_FACTOR 1
+#define APPS_READ_DELAY_MS 10
+
+elapsedMillis CAN_Timer;
 
 void setup() {
     Serial.begin(9600);
