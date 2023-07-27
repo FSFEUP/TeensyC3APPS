5988c911800539c93a7402f7868bc09f15eb7080
Tiago Amorim <tiago.andre.amorim@gmail.com>
2023-07-27 12:46:14 +0100

    Update main.cpp
    Signed-off-by: Tiago Amorim <tiago.andre.amorim@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,6 @@ volatile bool transmissionEnabled = false;
 volatile bool R2D = false;
 volatile bool R2DOverride = false;
 
-int VALUE_apps = 0;
-
 extern FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 
 extern CAN_message_t statusRequest;
@@ -57,17 +55,88 @@ void playR2DSound() {
     delay(1000);
     digitalWrite(buzzerPin, LOW);
 }
-
 void setup() {
     Serial.begin(9600);
     pinMode(APPS_1_PIN, INPUT);
     pinMode(APPS_2_PIN, INPUT);
+
+    pinMode(buzzerPin, OUTPUT);
     canSetup();
+
+    r2dButton.attach(R2D_PIN, INPUT);
+    r2dButton.interval(0.1);
+
+    R2DStatus = IDLE;
+    R2DTimer = 0;
+
+    delay(STARTUP_DELAY_MS);
+
+    can1.write(disable);
+    can1.write(statusRequest);
+    can1.write(actualSpeedRequest);
+    can1.write(DCVoltageRequest);
+
+    displaySetup();
+
+#ifdef MAIN_DEBUG
+    LOG("Setup complete, Waiting for R2D\n");
+#endif
 }
 
 void loop() {
-    if (APPSTimer > APPS_READ_PERIOD_MS) {
-        (void)readApps();
-        APPSTimer = 0;
+    if (mainLoopPeriod < 10)
+        return;
+
+#ifdef DATA_LOGGING
+    write()
+#endif
+
+#if DATA_DISPLAY > 0
+        displayUpdate();
+#endif
+
+    switch (R2DStatus) {
+        case IDLE:
+            r2dButton.update();
+
+#ifdef R2D_DEBUG
+            LOG("R2D Button: %d\tR2D: %s", r2dButton.read(), R2D ? "MAINS OK" : "MAINS OFF");
+            Serial.print("\tR2D Timer: ");
+            Serial.println(R2DTimer);
+#endif
+
+            if ((r2dButton.fell() and R2D and R2DTimer < R2D_TIMEOUT) or R2DOverride) {
+#ifdef R2D_DEBUG
+                LOG("R2D OK, Switching to drive mode\n");
+#endif
+                playR2DSound();
+                initBamocarD3();
+                R2DStatus = DRIVING;
+                break;
+            }
+            break;
+
+        case DRIVING:
+            if (not R2D and not R2DOverride) {
+                R2DStatus = IDLE;
+                can1.write(disable);
+                break;
+            }
+
+            if (APPSTimer > APPS_READ_PERIOD_MS) {
+                APPSTimer = 0;
+                int apps_value = readApps();
+
+                if (apps_value >= 0)
+                    sendMsg(apps_value);
+                else
+                    sendMsg(0);
+                break;
+            }
+            break;
+
+        default:
+            ERROR("Invalid r2d_status");
+            break;
     }
 }
